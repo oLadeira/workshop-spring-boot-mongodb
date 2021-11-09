25d04b3a8e31aeb9536535ef63855b86c438c4c4
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-11-09 16:58:37 -0300

    user insert with POST

--- src/main/java/com/lucasladeira/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasladeira/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasladeira.workshopmongo.domain.User;
+import com.lucasladeira.workshopmongo.dto.UserDTO;
 import com.lucasladeira.workshopmongo.repositories.UserRepository;
 import com.lucasladeira.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -29,4 +30,11 @@ public class UserService{
 		return user;
 	}
 	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
