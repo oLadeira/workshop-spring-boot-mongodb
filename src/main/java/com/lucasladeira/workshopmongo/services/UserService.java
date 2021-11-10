2886a58ecd13e0260d1f5b1aa699b1baca609f4d
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-11-10 10:45:46 -0300

    user DELETE

--- src/main/java/com/lucasladeira/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasladeira/workshopmongo/services/UserService.java
@@ -34,6 +34,12 @@ public class UserService{
 		return userRepository.insert(user);
 	}
 	
+	public void deleteById(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+	
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
